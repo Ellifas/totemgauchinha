69be2d527fc210104860fae93785ab6a4ef900fc
Ellifas <ellifasr@outlook.com>
2025-02-21 21:45:28 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+const ProductPage = () => {
+  return (
+    <div className="p-5 border-red-500 rounded-xl">
+      <h1 className="text-yellow-300">Products Page</h1>
+      <Button> SUPERLIAS </Button>
+      <Input placeholder="SAlve papai fecha ai" />
+    </div>
+  );
+};
+
+export default ProductPage;
